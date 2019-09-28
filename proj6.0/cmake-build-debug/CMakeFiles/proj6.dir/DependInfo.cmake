b741e95a24a8c28bbaaab34a261493f10912a006
Em4ML <emmamov121@gmail.com>
2019-09-28 15:26:37 +0400

    dynamic arrays

--- proj6.0/cmake-build-debug/CMakeFiles/proj6.dir/DependInfo.cmake
+++ proj6.0/cmake-build-debug/CMakeFiles/proj6.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/emma/CLionProjects/proj6/main.cpp" "/Users/emma/CLionProjects/proj6/cmake-build-debug/CMakeFiles/proj6.dir/main.cpp.o"
+  "/Users/emma/CLionProjects/proj6.0/main.cpp" "/Users/emma/CLionProjects/proj6.0/cmake-build-debug/CMakeFiles/proj6.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
